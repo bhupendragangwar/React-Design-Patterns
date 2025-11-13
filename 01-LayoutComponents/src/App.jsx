ea955f54e10c84d8eb547d34f1c8cc8cfef8814d
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-11-13 13:03:21 +0000

    Advanced React Design Component

--- 01-LayoutComponents/src/App.jsx
+++ 01-LayoutComponents/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { SplitScreen } from './components/split-screen'
+
+const LeftSideComp = ()=>{
+  return(
+    <h1 style={{backgroundColor: 'crimson'}}>I'm Left</h1>
+  )
+}
+
+const RightSideComp=()=>{
+  return(
+    <h1 style={{backgroundColor: 'burlywood'}}>I'm Right</h1>
+  )
+}
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <SplitScreen Left={LeftSideComp} Right={RightSideComp}/>
   )
 }
 
